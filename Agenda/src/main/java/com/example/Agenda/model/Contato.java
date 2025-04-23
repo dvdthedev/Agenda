5e54cead7cc6ef4c440bb58cea87ec0e1a7041c2
Deivid Alves <dvd2.0@live.com>
2025-04-23 14:47:14 -0300

    adicionando pasta

---
+++ Agenda/src/main/java/com/example/Agenda/model/Contato.java
@@ -0,0 +1,43 @@
+package com.example.Agenda.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.hibernate.Length;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "contatos")
+public class Contato implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idcon")
+    private Long idcon;
+    @Column(length = 120)
+    private String nome;
+    @Column(length = 11)
+    private String fone;
+    private String email;
+    @Column(length = 30)
+    private String apelido;
+    @Column(name = "data_criacao", updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataCriacao;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Contato contato = (Contato) o;
+        return Objects.equals(idcon, contato.idcon);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(idcon);
+    }
+}
---
+++ Agenda/src/main/resources/db.migration/V2__add_column_apelado.sql
@@ -0,0 +1,1 @@
+ALTER TABLE contatos ADD COLUMN apelido varchar(100) NOT NULL;
